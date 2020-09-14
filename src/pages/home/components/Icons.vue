3af1fb87d1a937011a3af72b383ece8facfc9b94
RussellGe <russellxin0601@gmail.com>
2020-09-14 16:28:52 +0800

    add recommends

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -9,8 +9,7 @@
       <p class="icon-desc">{{item.desc}}</p>
     </div></swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
-    </swiper>
-    
+  </swiper>
   </div>
 </template>
 
@@ -18,64 +17,62 @@
 export default {
   name: 'HomeIcons',
   data () {
-      return {
-          iconList: [{
-              id: '0001',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png",
-              desc: '景点门票'
-          },{
-              id: '0002',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/69e8b07cb2d438c5530aebd4c8e3abd3.png",
-              desc: '一日游'
-          },
-          {
-              id: '0003',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/b7a5e6b1774eda4fc9e32588f1d00c94.png",
-              desc: '踏青赏花'
-          },
-          {
-              id: '0004',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/c4c5c620f20535ff358788a508c20921.png",
-              desc: '主题乐园'
-          },{
-              id: '0005',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/59bc59a3c38261e9a3f2f991a9ce0d21.png",
-              desc: '深圳必游'
-          },{
-              id: '0006',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/931f626a44f714d055f9a0f8235170fa.png",
-              desc: '海滨海岛'
-          },{
-              id: '0007',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/557ba25df01d7dbf1419b095a11d1319.png",
-              desc: '泡温泉'
-          },{
-              id: '0008',
-              imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png",
-              desc: '城市观光'
-          },{
-              id: '0009',
-              imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png",
-              desc: '高空景观'
-          },{
-              id: '0010',
-              imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png",
-              desc: '自然风光'
-          },]
-      }
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/158387fe5376294f3776d01358d6b73b.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/69e8b07cb2d438c5530aebd4c8e3abd3.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/b7a5e6b1774eda4fc9e32588f1d00c94.png',
+        desc: '踏青赏花'
+      }, {
+        id: '0004',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/c4c5c620f20535ff358788a508c20921.png',
+        desc: '主题乐园'
+      }, {
+        id: '0005',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/59bc59a3c38261e9a3f2f991a9ce0d21.png',
+        desc: '深圳必游'
+      }, {
+        id: '0006',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/931f626a44f714d055f9a0f8235170fa.png',
+        desc: '海滨海岛'
+      }, {
+        id: '0007',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20209/557ba25df01d7dbf1419b095a11d1319.png',
+        desc: '泡温泉'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '城市观光'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '高空景观'
+      }, {
+        id: '0010',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '自然风光'
+      } ]
+    }
   },
   computed: {
-      pages () {
-          const pages = []
-          this.iconList.forEach((item,index)=> {
-              const page = Math.floor(index/8)
-              if(!pages[page]){
-                  pages[page] = []
-              }
-              pages[page].push(item)
-          })
-          return pages
-      }
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
   }
 }
 </script>
---
+++ src/pages/home/components/Recommand.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+      <div class="recommand-title">热销推荐</div>
+      <ul>
+        <li class="item" v-for='item of recommendList' :key='item.id'>
+        <div class="item-img-wrapper">
+            <img  class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+        </div>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommand',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/2e/d9363ce50a88c8c5ff5a138c67687cd4.water.jpg_200x200_1c15c8f5.jpg',
+        title: '世界之窗',
+        desc: '全球微缩景观，精彩演绎娱乐项目应有尽有'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/f2/f2e0213052717f7ca3.water.jpg_200x200_816c1b48.jpg',
+        title: '世界之窗',
+        desc: '全球微缩景观，精彩演绎娱乐项目应有尽有'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/f2/f2e0213052717f7ca3.water.jpg_200x200_816c1b48.jpg',
+        title: '世界之窗',
+        desc: '全球微缩景观，精彩演绎娱乐项目应有尽有'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1902/f2/f2e0213052717f7ca3.water.jpg_200x200_816c1b48.jpg',
+        title: '世界之窗',
+        desc: '全球微缩景观，精彩演绎娱乐项目应有尽有'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixin.styl'
+  .recommand-title
+    line-height: .8rem
+    background: #eeeeee
+    text-indent: .2rem
+    margin-top: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    overflow: hidden
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        color: #ffffff
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+
+</style>
